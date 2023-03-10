4ce113c58433909805d1698011706e639518185d
Manjeet Singh Bhatia <manjeet.s.bhatia@intel.com>
2023-03-10 22:54:49 +0100

    update cpu app

--- cpuapp/verify_hash.c
+++ cpuapp/verify_hash.c
@@ -4,62 +4,61 @@
 #include <openssl/evp.h>
 #include <openssl/bio.h>
 #include <openssl/pem.h>
+#include <omp.h>
 
-#define MESSAGE "pajeets"
-#define MESSAGE_LEN strlen(MESSAGE)
+#define MAX_SIZE 100000
 
-void handleErrors(void)
+void handlefaults(void)
 {
-    printf("An error occurred\n");
+    // fault handling will go here, its Simple for now.
+    printf("An excepted error occurred\n");
     exit(1);
 }
 
 
+// Generate hash of an integer message
+int generate_hash(int *message, size_t message_len, unsigned char *hash_res){
 
-int sign(unsigned char *message, size_t message_len, EVP_PKEY *pkey, unsigned char **sig, size_t *sig_len)
-{
-    // Generate a SHA-256 hash of the message
     EVP_MD_CTX *mdctx;
     const EVP_MD *md;
-    unsigned char md_value[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
     md = EVP_sha256();
+    int md_len;
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, md, NULL);
     EVP_DigestUpdate(mdctx, message, message_len);
-    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+    EVP_DigestFinal_ex(mdctx, hash_res, &md_len);
     EVP_MD_CTX_free(mdctx);
+    return md_len;
 
-    // Sign the hash
+}
+
+// sign the hash and store it in md_value.
+int sign(unsigned char *md_value, unsigned int md_len, EVP_PKEY *pkey, unsigned char **sig, size_t *sig_len)
+{
     EVP_MD_CTX *mdctx_sign;
     mdctx_sign = EVP_MD_CTX_new();
+    const EVP_MD *md;
+    md = EVP_sha256();
     EVP_SignInit(mdctx_sign, md);
     EVP_SignUpdate(mdctx_sign, md_value, md_len);
     *sig = malloc(EVP_PKEY_size(pkey));
     if (!EVP_SignFinal(mdctx_sign, *sig, sig_len, pkey)) {
-        handleErrors();
+        handlefaults();
     }
     EVP_MD_CTX_free(mdctx_sign);
 
     return 1;
 }
 
-int verify(unsigned char *message, size_t message_len, unsigned char *sig, size_t sig_len, EVP_PKEY *pkey)
+
+// verfiy the hash with signature
+int verify(unsigned char *md_value, unsigned int md_len, unsigned char *sig, size_t sig_len, EVP_PKEY *pkey)
 {
-    // Generate a SHA-256 hash of the message
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
-    unsigned char md_value[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
-    md = EVP_sha256();
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, message, message_len);
-    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
-    EVP_MD_CTX_free(mdctx);
 
     EVP_MD_CTX *mdctx_verify;
     mdctx_verify = EVP_MD_CTX_new();
+    const EVP_MD *md;
+    md = EVP_sha256();
     EVP_VerifyInit(mdctx_verify, md);
     EVP_VerifyUpdate(mdctx_verify, md_value, md_len);
     int verified;
@@ -83,19 +82,38 @@ int main(void)
     RSA *rsa;
     rsa = RSA_generate_key(2048, RSA_F4, NULL, NULL);
     EVP_PKEY_assign_RSA(pkey, rsa);
+    
 
+    //unsigned char *sig;
+    //size_t sig_len;
+    //unsigned char md_value[EVP_MAX_MD_SIZE];
+    //unsigned int md_len = 0;
+    int message =0;
+    //unsigned char *sig;
+    //size_t sig_len;
+    //size_t message_len = sizeof(int);
+    double start, end;
+
+    start = omp_get_wtime();
+    
+    #pragma omp parallel private(message)
+    #pragma omp for
+    for (message=0; message<MAX_SIZE; message++){
+    unsigned char *msg_bytes = (unsigned char *)&message;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
     unsigned char *sig;
     size_t sig_len;
-    if (!sign((unsigned char *)MESSAGE, MESSAGE_LEN, pkey, &sig, &sig_len)) {
-        handleErrors();
+    size_t message_len = sizeof(int);
+
+
+    md_len = generate_hash((int *)msg_bytes, message_len, md_value);
+    if (!sign(md_value, md_len, pkey, &sig, &sig_len)) {
+        handlefaults();
     } 
-
-
-    printf("Signature:\n");
-    BIO_dump_fp(stdout, (const char *)sig, sig_len);
-
+     
     int verified;
-    verified = verify((unsigned char *)MESSAGE, MESSAGE_LEN, sig, sig_len, pkey);
+    verified = verify(md_value, md_len, sig, sig_len, pkey);
 
     // Print the verification result
     if (verified == 1) {
@@ -103,13 +121,18 @@ int main(void)
     } else if (verified == 0) {
         printf("Verification failed!\n");
     } else {
-        handleErrors();
+        handlefaults();
     }
 
-    // Clean up
     free(sig);
+}
+
+    // Clean up
+    //free(sig);
     EVP_PKEY_free(pkey);
     EVP_cleanup();
+    end = omp_get_wtime();
+    printf("Time taken is for generating hashes and signing %f seconds\n", end-start);
 
     return 0;
 }
